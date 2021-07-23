d1bf19c059d1d6c700e909521b78b676a6142443
Sebastian Ghetu <sebastian@ghetu.net>
2021-07-23 22:23:53 +0100

    Notes for the interviewer

--- Clearscore TestUITests/Features/Score/MainViewTests.swift
+++ Clearscore TestUITests/Features/Score/MainViewTests.swift
@@ -7,6 +7,13 @@
 
 import XCTest
 
+// Notes for interviewer: These tests are quite basic. In production I would've stubbed the network responses,
+// so that I could test all view states.
+//
+// I find this Robots abstraction really useful when writing UI tests, as it encourages whoever is writing them to
+// think more in terms of accessibility rather than testing out scenarios which can easily be picked up by unit tests.
+//
+
 class MainViewTests: BaseUITest {
     
     func test_mainViewController_isAccessible() {

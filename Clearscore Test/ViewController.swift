d3c44f9a70b5b9a491f48c1f5f09a346fcd628cb
Sebastian Ghetu <sebastian@ghetu.net>
2021-07-23 01:15:28 +0100

    Populating localized strings and animating the score view

--- Clearscore Test/ViewController.swift
+++ Clearscore Test/ViewController.swift
@@ -9,8 +9,10 @@ import Combine
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var cancellable: AnyCancellable?
+    
+    @IBOutlet weak var contentWrapperView: UIStackView!
     
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var scoreView: ScoreView!
@@ -19,17 +21,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        scoreView.animationCompletionHandler = { [unowned self] in
-            UIView.animate(withDuration: 0.3) {
-                self.button.isHidden = false
-                self.stackView.layoutIfNeeded()
-            }
-        }
-        
         self.cancellable = CreditService.getCreditData().sink { completion in
             print("Completed")
-        } receiveValue: { response in
-            print("\(response)")
+        } receiveValue: { [weak self] response in
+            guard let self = self else { return }
+                        
+            let scoreTextWithPlaceholder = NSLocalizedString("scoreView.scoreFormattable", comment: "")
+            let formattedScoreText = String(format: scoreTextWithPlaceholder, response.creditReportInfo.score)
+            
+            let maxScoreTextWithPlaceholder = NSLocalizedString("scoreView.maxScoreFormattable", comment: "")
+            let formattedMaxScoreText = String(format: maxScoreTextWithPlaceholder, response.creditReportInfo.maxScoreValue)
+            
+            let viewModel = ScoreViewModel(
+                scoreIntroText: NSLocalizedString("scoreView.intro", comment: ""),
+                score: response.creditReportInfo.score,
+                scoreText: formattedScoreText,
+                maxScore: response.creditReportInfo.maxScoreValue,
+                maxScoreText: formattedMaxScoreText,
+                scoreStatusText: NSLocalizedString("scoreView.status.bright", comment: ""))
+            
+            self.scoreView.viewModel = viewModel
+            
+            self.contentWrapperView.addArrangedSubview(self.stackView)
+
+            self.scoreView.animationCompletionHandler = { [unowned self] in
+                UIView.animate(withDuration: 0.3) {
+                    self.button.isHidden = false
+                    self.button.layoutIfNeeded()
+                }
+            }
         }
     }
 }
